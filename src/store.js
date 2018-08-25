3aca76671abca412c7e07c11e33a037659745aac
Nikita Khristinin <nkhristinin@gmail.com>
2018-08-25 18:00:16 +0300

    Add redux thunk

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
 import rootReducer from "./modules"
 
 const initialState = {}
 const enhancers = []
-const middleware = []
+const middleware = [thunk]
 
 if (process.env.NODE_ENV === "development") {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
